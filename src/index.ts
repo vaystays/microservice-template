6141e05b7542613e6da16937f2885d59621c4aa5
Tyler Garlick <tjgarlick@gmail.com>
2019-12-26 08:34:12 -0700

    Preliminary template setup

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { GraphQLServer } from 'graphql-yoga'
+import * as Decimal from 'graphql-type-decimal'
+import * as Email from 'graphql-type-email'
+import * as UUID from 'graphql-type-uuid'
+import * as JSON from 'graphql-type-json'
+
+const resolvers = {
+  Decimal,
+  Email,
+  UUID,
+  JSON,
+
+  Query: {
+    async health() {
+      return {
+        type: 'OK',
+        payload: {
+          package: require('../package.json')
+
+        }
+      }
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: [
+    // './schemas/scalars.graphql',
+    // './schemas/enums.graphql',
+    './schemas/root.graphql'
+  ],
+  resolvers
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
